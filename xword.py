1f3015c05e5f24c27a4981697174eab1c5083560
Karen Marvel <knmarvel@gmail.com>
2020-04-13 10:55:44 -0400

    crossword solver functioning

--- xword.py
+++ xword.py
@@ -2,21 +2,37 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "knmarvel"
+
+import re
 
 # YOUR HELPER FUNCTION GOES HERE
+def change_to_regex(test_word):
+    re_str = "^"
+    for letter in test_word:
+        if letter == " ":
+            re_str += "."
+        else:
+            re_str += f'({letter})'
+    return re_str + "$"
+
+
+def find_matches(re_str, words):
+    matches = []
+    for word in words:
+        if re.findall(re_str, word):
+            matches.append(word)
+    return matches
 
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
 
-    test_word = raw_input(
+    test_word = input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
-
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
-
+    re_str = change_to_regex(test_word)
+    print("\n".join(find_matches(re_str, words)))
 
 if __name__ == '__main__':
     main()
